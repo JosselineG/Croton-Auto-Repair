6cf8b6254233c0eb2d89f40d8812a0c1daf547a8
Josseline <guardadojosseline@gmail.com>
2023-07-19 13:28:44 -0400

    changes to service component

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -12,7 +12,7 @@ import './Services.css'
 const Services = () => {
   return (
     <div className='services'>
-  <motion.div
+      <motion.div
         className="box"
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -22,33 +22,38 @@ const Services = () => {
           ease: [0, 0.71, 0.2, 1.01]
         }}
       >
-      <div className='List-image'>
+        <div className='List-image'>
 
-        <div className='List'>
-          <h1> OUR SERVICES</h1>
-          <span>
+          <div className='List'>
+            <h1> OUR SERVICES</h1>
+           
+              <h2>Complete Auto Repair shop, we specialize
+               in domestic and imported vehicles.
+              </h2>
+           
+            <span>
 
-            <li>Towing Services</li>
-            <li>Brakes</li>
-            <li>Tune-ups</li>
-            <li>Oil Changes</li>
-            <li>General Service</li>
-            <li>Import Specialist</li>
-            <li>A/C Service</li>
-            <li>Inspection</li>
-            <li>Car Audio & Video Service</li>
+              <li>Towing Services</li>
+              <li>Brakes</li>
+              <li>Tune-ups</li>
+              <li>Oil Changes</li>
+              <li>General Service</li>
+              <li>Import Specialist</li>
+              <li>A/C Service</li>
+              <li>Inspection</li>
+              <li>Car Audio & Video Service</li>
 
-          </span>
+            </span>
+          </div>
+
+          <div className='images'>
+            <Image className='AImg' src={AImg} alt='Tow Service' />
+            <Image className='BImg' src={BImg} alt='Car Lift' />
+            <Image className='CImg' src={CImg} alt='Car' />
+
+          </div>
+
         </div>
-
-        <div className='images'>
-          <Image className='AImg' src={AImg} alt='Tow Service' />
-          <Image className='BImg' src={BImg} alt='Car Lift' />
-          <Image className='CImg' src={CImg} alt='Car' />
-        
-        </div>
-
-      </div>
       </motion.div>
     </div>
   )
